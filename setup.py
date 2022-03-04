c727ae9103f3fabe602a9dbe76295bd3a91cc877
Jaap <jaap1@me.com>
2022-03-04 10:23:11 +0100

    Release adjustements

--- setup.py
+++ setup.py
@@ -3,22 +3,34 @@ from setuptools import find_packages, setup
 setup(
     name="teamleaderApiV1",
     version="0.4",
-    packages=find_packages(),
-    url="",
-    license="Apache 2.0",
-    author="Jaap van den Broek",
-    author_email="jaap@rechtdirect.nl",
+    url="https://github.com/HeroPP/teamleaderApiV1",
+    license="MIT",
+    author="Jaap",
+    author_email="jaap1@me.com",
     description="Python framework on top of the teamleader v1 api",
-    classifiers=[
-        'Programming Language :: Python :: 3.8',
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires=">=3.8",
     install_requires=[
-        "requests",
+        "certifi",
+        "chardet",
+        "idna",
         "pickle5",
         "ratelimit",
-        "requests_oauthlib"
+        "requests",
+        "urllib3",
+    ],
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        # Pick your license as you wish
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
     ],
 )
